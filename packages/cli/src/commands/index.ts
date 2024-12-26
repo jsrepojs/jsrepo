ac3eb5ca55a8126c9922e5ef771f950d16feef23
Aidan Bleser <aidanbleser35@gmail.com>
2024-12-26 08:00:16 -0600

    feat: Add `exec` command

--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -2,8 +2,9 @@ import { add } from './add';
 import { auth } from './auth';
 import { build } from './build';
 import { diff } from './diff';
+import { exec } from './exec';
 import { init } from './init';
 import { test } from './test';
 import { update } from './update';
 
-export { add, auth, build, diff, init, test, update };
+export { add, auth, build, diff, exec, init, test, update };
