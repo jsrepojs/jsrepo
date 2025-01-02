6ec9cabb3846ff99a1eada33638f288ecfd1fc4f
Aidan Bleser <aidanbleser35@gmail.com>
2025-01-02 06:28:14 -0600

    docs: Fix docs demos layout

--- sites/docs/src/routes/demos/+page.svelte
+++ sites/docs/src/routes/demos/+page.svelte
@@ -4,39 +4,55 @@
 </svelte:head>
 
 <main class="flex justify-center px-4" style="min-height: calc(100svh - 56px);">
-	<div class="max-w-2xl content-center w-full py-8 gap-5 flex flex-col">
-		<h1 class="text-4xl font-bold text-center">Demos</h1>
-		<iframe
-			class="aspect-video w-full"
-			src="https://www.youtube.com/embed/IyJQI3z8PWg?si=zhg54w9mE97s6K9c"
-			title="YouTube video player"
-			frameborder="0"
-			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-			referrerpolicy="strict-origin-when-cross-origin"
-			allowfullscreen
-		></iframe>
-		<h2 class="text-2xl font-bold text-center">The features I am most excited about in jsrepo</h2>
-		<iframe
-			class="aspect-video w-full"
-			src="https://www.youtube.com/embed/6YocZSwjUW8?si=3ibzsmqTu6-IcOWS"
-			title="YouTube video player"
-			frameborder="0"
-			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-			referrerpolicy="strict-origin-when-cross-origin"
-			allowfullscreen
-		></iframe>
-		<h2 class="text-2xl font-bold text-center">
-			Creating your own shadcn-style library with jsrepo
-		</h2>
-		<iframe
-			width="560"
-			height="315"
-			src="https://www.youtube.com/embed/zWfBt1vKb84?si=_pbJ75Ww-tOX_Lui"
-			title="YouTube video player"
-			frameborder="0"
-			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-			referrerpolicy="strict-origin-when-cross-origin"
-			allowfullscreen
-		></iframe>
+	<div class="grid sm:grid-cols-2 w-full py-8 gap-5 flex-col h-fit">
+		<div class="border border-border rounded-lg max-w-[500px] overflow-hidden">
+			<iframe
+				class="aspect-video w-full"
+				src="https://www.youtube.com/embed/IyJQI3z8PWg?si=zhg54w9mE97s6K9c"
+				title="YouTube video player"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				referrerpolicy="strict-origin-when-cross-origin"
+				allowfullscreen
+			></iframe>
+			<div class="p-4 border-t border-border">
+				<h4>Creating your first registry with jsrepo</h4>
+				<span class="text-muted-foreground"> How to create your first registry with jsrepo. </span>
+			</div>
+		</div>
+		<div class="border border-border rounded-lg max-w-[500px] overflow-hidden">
+			<iframe
+				class="aspect-video w-full"
+				src="https://www.youtube.com/embed/6YocZSwjUW8?si=3ibzsmqTu6-IcOWS"
+				title="YouTube video player"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				referrerpolicy="strict-origin-when-cross-origin"
+				allowfullscreen
+			></iframe>
+			<div class="p-4 border-t border-border">
+				<h4>The features I am most excited about in jsrepo</h4>
+				<span class="text-muted-foreground">
+					Some new and existing features that make jsrepo awesome.
+				</span>
+			</div>
+		</div>
+		<div class="border border-border rounded-lg max-w-[500px] overflow-hidden">
+			<iframe
+				class="aspect-video w-full"
+				src="https://www.youtube.com/embed/zWfBt1vKb84?si=_pbJ75Ww-tOX_Lui"
+				title="YouTube video player"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				referrerpolicy="strict-origin-when-cross-origin"
+				allowfullscreen
+			></iframe>
+			<div class="p-4 border-t border-border">
+				<h4>Creating your own shadcn-style library with jsrepo</h4>
+				<span class="text-muted-foreground">
+					How to create a shadcn-style ui library with jsrepo.
+				</span>
+			</div>
+		</div>
 	</div>
 </main>
