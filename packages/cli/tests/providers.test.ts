d27c99a6f019fc404396e5dd1b5ed4afcb3761c1
Aidan Bleser <aidanbleser35@gmail.com>
2025-01-16 06:16:45 -0600

    add tests for http

--- packages/cli/tests/providers.test.ts
+++ packages/cli/tests/providers.test.ts
@@ -804,3 +804,308 @@ describe('azure', () => {
 `);
 	});
 });
+
+describe('http', () => {
+	it('Fetches the manifest', async () => {
+		const repoURL = 'https://jsrepo-http.vercel.app';
+
+		const info = await gitProviders.http.info(repoURL);
+
+		const content = await gitProviders.http.fetchRaw(info, 'jsrepo-manifest.json');
+
+		expect(content.unwrap()).toBe(`[
+	{
+		"name": "types",
+		"blocks": [
+			{
+				"name": "result",
+				"directory": "src/types",
+				"category": "types",
+				"tests": true,
+				"subdirectory": false,
+				"list": true,
+				"files": [
+					"result.ts",
+					"result.test.ts"
+				],
+				"localDependencies": [],
+				"_imports_": {},
+				"dependencies": [],
+				"devDependencies": []
+			}
+		]
+	},
+	{
+		"name": "utils",
+		"blocks": [
+			{
+				"name": "array-sum",
+				"directory": "src/utils",
+				"category": "utils",
+				"tests": true,
+				"subdirectory": false,
+				"list": true,
+				"files": [
+					"array-sum.ts",
+					"array-sum.test.ts"
+				],
+				"localDependencies": [],
+				"_imports_": {},
+				"dependencies": [],
+				"devDependencies": []
+			},
+			{
+				"name": "array-to-map",
+				"directory": "src/utils",
+				"category": "utils",
+				"tests": true,
+				"subdirectory": false,
+				"list": true,
+				"files": [
+					"array-to-map.ts",
+					"array-to-map.test.ts"
+				],
+				"localDependencies": [],
+				"_imports_": {},
+				"dependencies": [],
+				"devDependencies": []
+			},
+			{
+				"name": "dispatcher",
+				"directory": "src/utils",
+				"category": "utils",
+				"tests": true,
+				"subdirectory": false,
+				"list": true,
+				"files": [
+					"dispatcher.ts",
+					"dispatcher.test.ts"
+				],
+				"localDependencies": [],
+				"_imports_": {},
+				"dependencies": [],
+				"devDependencies": []
+			},
+			{
+				"name": "ipv4-address",
+				"directory": "src/utils",
+				"category": "utils",
+				"tests": true,
+				"subdirectory": false,
+				"list": true,
+				"files": [
+					"ipv4-address.ts",
+					"ipv4-address.test.ts"
+				],
+				"localDependencies": [
+					"types/result",
+					"utils/is-number"
+				],
+				"_imports_": {
+					"../types/result": "{{types/result}}",
+					"./is-number": "{{utils/is-number}}"
+				},
+				"dependencies": [],
+				"devDependencies": []
+			},
+			{
+				"name": "is-number",
+				"directory": "src/utils",
+				"category": "utils",
+				"tests": true,
+				"subdirectory": false,
+				"list": true,
+				"files": [
+					"is-number.ts",
+					"is-number.test.ts"
+				],
+				"localDependencies": [],
+				"_imports_": {},
+				"dependencies": [],
+				"devDependencies": []
+			},
+			{
+				"name": "lines",
+				"directory": "src/utils",
+				"category": "utils",
+				"tests": true,
+				"subdirectory": false,
+				"list": true,
+				"files": [
+					"lines.ts",
+					"lines.test.ts"
+				],
+				"localDependencies": [
+					"utils/pad"
+				],
+				"_imports_": {
+					"./pad": "{{utils/pad}}"
+				},
+				"dependencies": [],
+				"devDependencies": []
+			},
+			{
+				"name": "map-to-array",
+				"directory": "src/utils",
+				"category": "utils",
+				"tests": true,
+				"subdirectory": false,
+				"list": true,
+				"files": [
+					"map-to-array.ts",
+					"map-to-array.test.ts"
+				],
+				"localDependencies": [],
+				"_imports_": {},
+				"dependencies": [],
+				"devDependencies": []
+			},
+			{
+				"name": "math",
+				"directory": "src/utils/math",
+				"category": "utils",
+				"tests": true,
+				"subdirectory": true,
+				"list": true,
+				"files": [
+					"circle.test.ts",
+					"circle.ts",
+					"conversions.test.ts",
+					"conversions.ts",
+					"fractions.test.ts",
+					"fractions.ts",
+					"gcf.test.ts",
+					"gcf.ts",
+					"index.ts",
+					"triangles.test.ts",
+					"triangles.ts",
+					"types.ts"
+				],
+				"localDependencies": [],
+				"dependencies": [],
+				"devDependencies": [],
+				"_imports_": {}
+			},
+			{
+				"name": "pad",
+				"directory": "src/utils",
+				"category": "utils",
+				"tests": true,
+				"subdirectory": false,
+				"list": true,
+				"files": [
+					"pad.ts",
+					"pad.test.ts"
+				],
+				"localDependencies": [
+					"utils/strip-ansi"
+				],
+				"_imports_": {
+					"./strip-ansi": "{{utils/strip-ansi}}"
+				},
+				"dependencies": [],
+				"devDependencies": []
+			},
+			{
+				"name": "perishable-list",
+				"directory": "src/utils",
+				"category": "utils",
+				"tests": true,
+				"subdirectory": false,
+				"list": true,
+				"files": [
+					"perishable-list.ts",
+					"perishable-list.test.ts"
+				],
+				"localDependencies": [],
+				"_imports_": {},
+				"dependencies": [],
+				"devDependencies": []
+			},
+			{
+				"name": "rand",
+				"directory": "src/utils",
+				"category": "utils",
+				"tests": true,
+				"subdirectory": false,
+				"list": true,
+				"files": [
+					"rand.ts",
+					"rand.test.ts"
+				],
+				"localDependencies": [],
+				"_imports_": {},
+				"dependencies": [],
+				"devDependencies": []
+			},
+			{
+				"name": "sleep",
+				"directory": "src/utils",
+				"category": "utils",
+				"tests": true,
+				"subdirectory": false,
+				"list": true,
+				"files": [
+					"sleep.ts",
+					"sleep.test.ts"
+				],
+				"localDependencies": [],
+				"_imports_": {},
+				"dependencies": [],
+				"devDependencies": []
+			},
+			{
+				"name": "stopwatch",
+				"directory": "src/utils",
+				"category": "utils",
+				"tests": true,
+				"subdirectory": false,
+				"list": true,
+				"files": [
+					"stopwatch.ts",
+					"stopwatch.test.ts"
+				],
+				"localDependencies": [],
+				"_imports_": {},
+				"dependencies": [],
+				"devDependencies": []
+			},
+			{
+				"name": "strip-ansi",
+				"directory": "src/utils",
+				"category": "utils",
+				"tests": true,
+				"subdirectory": false,
+				"list": true,
+				"files": [
+					"strip-ansi.ts",
+					"strip-ansi.test.ts"
+				],
+				"localDependencies": [],
+				"_imports_": {},
+				"dependencies": [
+					"ansi-regex@^6.1.0"
+				],
+				"devDependencies": []
+			},
+			{
+				"name": "truncate",
+				"directory": "src/utils",
+				"category": "utils",
+				"tests": true,
+				"subdirectory": false,
+				"list": true,
+				"files": [
+					"truncate.ts",
+					"truncate.test.ts"
+				],
+				"localDependencies": [],
+				"_imports_": {},
+				"dependencies": [],
+				"devDependencies": []
+			}
+		]
+	}
+]`);
+	});
+});
