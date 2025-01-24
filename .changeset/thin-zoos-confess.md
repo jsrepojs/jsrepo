d6bc0eea374efa80047ed21ec1065df62a75c865
Aidan Bleser <aidanbleser35@gmail.com>
2025-01-24 10:04:07 -0600

    expose registry types

---
+++ .changeset/thin-zoos-confess.md
@@ -0,0 +1,5 @@
+---
+"jsrepo": patch
+---
+
+Expose registry types.
---
+++ packages/cli/src/registry/index.ts
@@ -0,0 +1,6 @@
+import type { Block, Category } from '../utils/build';
+import type { RegistryConfig, ProjectConfig } from '../utils/config';
+
+type Manifest = Category[];
+
+export type { Block, Category, Manifest, RegistryConfig, ProjectConfig };
