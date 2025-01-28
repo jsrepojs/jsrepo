a9ae00149225f5e93bbc7d0f77b99a7e13a6ef69
Aidan Bleser <aidanbleser35@gmail.com>
2025-01-28 14:23:45 -0600

    Update hooks.server.ts

--- sites/docs/src/hooks.server.ts
+++ sites/docs/src/hooks.server.ts
@@ -1,9 +1,4 @@
-import { markdownIt } from '$lib/ts/markdown';
-import { redirect, type ServerInit } from '@sveltejs/kit';
-
-export const init: ServerInit = async () => {
-	await markdownIt(); // creates the new instance
-};
+import { redirect } from '@sveltejs/kit';
 
 export const handle = async ({ event, resolve }) => {
 	if (event.url.pathname === '/about') throw redirect(303, '/docs/about');
