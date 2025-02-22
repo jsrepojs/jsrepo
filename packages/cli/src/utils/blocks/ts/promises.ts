7dcf515f662e4cfcac559b51b8756fd122cd9a32
Aidan Bleser <aidanbleser35@gmail.com>
2025-02-22 14:39:51 -0600

    Update promises.ts

--- packages/cli/src/utils/blocks/ts/promises.ts
+++ packages/cli/src/utils/blocks/ts/promises.ts
@@ -1,7 +1,18 @@
-/** Returns a promise that immediately resolves to `T`
+/*
+	jsrepo 1.39.3
+	Installed from github/ieedan/std
+	2-22-2025
+*/
+
+/** Returns a promise that immediately resolves to `T`, useful when you need to mix sync and async code.
+ *
+ * ### Usage
+ * ```ts
+ * const promises: Promise<number>[] = [];
+ *
+ * promises.push(noopPromise(10));
+ * ```
  *
  * @param val
  */
-export const noopPromise = <T>(val: T): Promise<T> => {
-	return new Promise<T>((res) => res(val));
-};
+export const noopPromise = <T>(val: T) => new Promise<T>((res) => res(val));
