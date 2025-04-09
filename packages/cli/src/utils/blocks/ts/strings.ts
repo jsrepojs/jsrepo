9811023f4b6088bd212c7a8998c3554e7e835f62
Aidan Bleser <aidanbleser35@gmail.com>
2025-04-09 09:37:12 -0500

    update std functions

--- packages/cli/src/utils/blocks/ts/strings.ts
+++ packages/cli/src/utils/blocks/ts/strings.ts
@@ -4,40 +4,55 @@
 
 /** Returns true if `str` starts with one of the provided `strings`.
  *
+ * @param str
+ * @param strings
+ * @returns
+ *
  * ## Usage
  * ```ts
  * startsWithOneOf('ab', 'a', 'c'); // true
  * startsWithOneOf('cc', 'a', 'b'); // false
  * ```
- *
- * @param str
- * @param strings
- * @returns
  */
-export const startsWithOneOf = (str: string, strings: string[]): boolean => {
+export function startsWithOneOf(str: string, strings: string[]): boolean {
 	for (const s of strings) {
 		if (str.startsWith(s)) return true;
 	}
 
 	return false;
-};
+}
 
 /** Returns true if `str` starts with one of the provided `strings`.
+ *
+ * @param str
+ * @param strings
+ * @returns
  *
  * ## Usage
  * ```ts
  * endsWithOneOf('cb', 'a', 'b'); // true
  * endsWithOneOf('cc', 'a', 'b'); // false
  * ```
- *
- * @param str
- * @param strings
- * @returns
  */
-export const endsWithOneOf = (str: string, strings: string[]): boolean => {
+export function endsWithOneOf(str: string, strings: string[]): boolean {
 	for (const s of strings) {
 		if (str.endsWith(s)) return true;
 	}
 
 	return false;
-};
+}
+
+/** Case insensitive equality. Returns true if `left.toLowerCase()` and `right.toLowerCase()` are equal.
+ *
+ * @param left
+ * @param right
+ * @returns
+ *
+ * ## Usage
+ * ```ts
+ * iEqual('Hello, World!', 'hello, World!'); // true
+ * ```
+ */
+export function iEqual(left: string, right: string): boolean {
+	return left.toLowerCase() === right.toLowerCase();
+}
--- packages/cli/src/utils/blocks/ts/url.ts
+++ packages/cli/src/utils/blocks/ts/url.ts
@@ -4,40 +4,44 @@
 
 /** Joins the segments into a single url correctly handling leading and trailing slashes in each segment.
  *
+ * @param segments
+ * @returns
+ *
  * ## Usage
  * ```ts
  * const url = join('https://example.com', '', 'api/', '/examples/');
  *
  * console.log(url); // https://example.com/api/examples
  * ```
- *
- * @param segments
- * @returns
  */
-const join = (...segments: string[]) => {
+export function join(...segments: string[]): string {
 	return segments
 		.map((s) => removeLeadingAndTrailingSlash(s))
 		.filter(Boolean)
 		.join('/');
-};
+}
 
 /** Removes the leading and trailing slash from the segment (if they exist)
+ *
+ * @param segment
+ * @returns
+ *
  * ## Usage
  * ```ts
  * const segment = removeLeadingAndTrailingSlash('/example/');
  *
  * console.log(segment); // 'example'
  * ```
+ */
+export function removeLeadingAndTrailingSlash(segment: string): string {
+	const newSegment = removeLeadingSlash(segment);
+	return removeTrailingSlash(newSegment);
+}
+
+/** Adds a leading and trailing to the beginning and end of the segment (if it doesn't already exist)
  *
  * @param segment
  * @returns
- */
-const removeLeadingAndTrailingSlash = (segment: string) => {
-	const newSegment = removeLeadingSlash(segment);
-	return removeTrailingSlash(newSegment);
-};
-
-/** Adds a leading and trailing to the beginning and end of the segment (if it doesn't already exist)
  *
  * ## Usage
  * ```ts
@@ -45,19 +49,19 @@ const removeLeadingAndTrailingSlash = (segment: string) => {
  *
  * console.log(segment); // '/example/'
  * ```
- *
- * @param segment
- * @returns
  */
-const addLeadingAndTrailingSlash = (segment: string) => {
+export function addLeadingAndTrailingSlash(segment: string): string {
 	// this is a weird case so feel free to handle it however you think it makes the most sense
 	if (segment === '') return '//';
 
 	const newSegment = addLeadingSlash(segment);
 	return addTrailingSlash(newSegment);
-};
+}
 
 /** Removes the leading slash from the beginning of the segment (if it exists)
+ *
+ * @param segment
+ * @returns
  *
  * ## Usage
  * ```ts
@@ -65,20 +69,20 @@ const addLeadingAndTrailingSlash = (segment: string) => {
  *
  * console.log(segment); // 'example'
  * ```
- *
- * @param segment
- * @returns
  */
-const removeLeadingSlash = (segment: string): string => {
+export function removeLeadingSlash(segment: string): string {
 	let newSegment = segment;
 	if (newSegment.startsWith('/')) {
 		newSegment = newSegment.slice(1);
 	}
 
 	return newSegment;
-};
+}
 
 /** Adds a leading slash to the beginning of the segment (if it doesn't already exist)
+ *
+ * @param segment
+ * @returns
  *
  * ## Usage
  * ```ts
@@ -86,20 +90,20 @@ const removeLeadingSlash = (segment: string): string => {
  *
  * console.log(segment); // '/example'
  * ```
- *
- * @param segment
- * @returns
  */
-const addLeadingSlash = (segment: string): string => {
+export function addLeadingSlash(segment: string): string {
 	let newSegment = segment;
 	if (!newSegment.startsWith('/')) {
 		newSegment = `/${newSegment}`;
 	}
 
 	return newSegment;
-};
+}
 
 /** Removes the trailing slash from the end of the segment (if it exists)
+ *
+ * @param segment
+ * @returns
  *
  * ## Usage
  * ```ts
@@ -107,19 +111,20 @@ const addLeadingSlash = (segment: string): string => {
  *
  * console.log(segment); // 'example'
  * ```
- * @param segment
- * @returns
  */
-const removeTrailingSlash = (segment: string): string => {
+export function removeTrailingSlash(segment: string): string {
 	let newSegment = segment;
 	if (newSegment.endsWith('/')) {
 		newSegment = newSegment.slice(0, newSegment.length - 1);
 	}
 
 	return newSegment;
-};
+}
 
 /** Adds a trailing slash to the end of the segment (if it doesn't already exist)
+ *
+ * @param segment
+ * @returns
  *
  * ## Usage
  * ```ts
@@ -127,20 +132,19 @@ const removeTrailingSlash = (segment: string): string => {
  *
  * console.log(segment); // 'example/'
  * ```
- *
- * @param segment
- * @returns
  */
-const addTrailingSlash = (segment: string): string => {
+export function addTrailingSlash(segment: string): string {
 	let newSegment = segment;
 	if (!newSegment.endsWith('/')) {
 		newSegment = `${newSegment}/`;
 	}
 
 	return newSegment;
-};
+}
 
 /** Removes the last segment of the url.
+ *
+ * @param url
  *
  * ## Usage
  * ```ts
@@ -148,24 +152,11 @@ const addTrailingSlash = (segment: string): string => {
  *
  * console.log(url); // '/first'
  * ```
- *
- * @param url
  */
-const upOneLevel = (url: string): string => {
+export function upOneLevel(url: string): string {
 	if (url === '/') return url;
 
 	const lastIndex = removeTrailingSlash(url).lastIndexOf('/');
 
 	return url.slice(0, url.length - lastIndex - 1);
-};
-
-export {
-	join,
-	removeLeadingSlash,
-	removeTrailingSlash,
-	addTrailingSlash,
-	addLeadingSlash,
-	addLeadingAndTrailingSlash,
-	removeLeadingAndTrailingSlash,
-	upOneLevel,
-};
+}
