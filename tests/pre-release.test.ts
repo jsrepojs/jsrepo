a22c00de49bbf4631bc555eb6a0b1410f559df62
Aidan Bleser <aidanbleser35@gmail.com>
2025-05-04 07:00:47 -0500

    Update pre-release.test.ts

--- tests/pre-release.test.ts
+++ tests/pre-release.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import { BASE_URL as JSREPO_BASE_URL } from '../src/utils/registry-providers/jsrepo';
 
+// just here to prevent me from shooting myself in the foot
 describe('JSREPO_BASE_URL', () => {
 	it('is the correct url', () => {
 		expect(JSREPO_BASE_URL).toBe('https://jsrepo.com');
